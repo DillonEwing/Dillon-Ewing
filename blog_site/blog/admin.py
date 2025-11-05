4175ad19b3d71d15e2b3a575e4f013da6ce534cf
Ewing <ewing@example.com>
2025-11-05 00:49:42 -0600

    Admin: register Category/Post/Comment with list_display, filters, search

--- blog_site/blog/admin.py
+++ blog_site/blog/admin.py
@@ -3,14 +3,25 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Category, Comment, Post
 
+
 class CategoryAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name',)
+    search_fields = ('name',)
+
 
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title', 'author', 'created_on', 'last_modified')
+    list_filter = ('categories', 'created_on', 'author')
+    search_fields = ('title', 'body')
+    raw_id_fields = ('author',)
+    filter_horizontal = ('categories',)
+
 
 class CommentAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('author', 'post', 'created_on')
+    list_filter = ('created_on', 'post')
+    search_fields = ('author', 'body')
+    raw_id_fields = ('post',)
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
